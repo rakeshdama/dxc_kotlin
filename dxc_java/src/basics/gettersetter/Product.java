7b39e3a9770b3b77ce0a2ad12ac5d2078a254389
rakeshdama <rakeshd0552@gmail.com>
2021-04-09 00:09:32 +0530

    This keyword and Getter Setters

--- dxc_java/src/basics/gettersetter/Product.java
+++ dxc_java/src/basics/gettersetter/Product.java
@@ -4,10 +4,13 @@ public class Product {
 	
 	protected String name; // here it is provate i.e only accessible within class
 	
-	void setName(String newName) {
+	void setName(String name) {
 		// here it is used to set the value
 		
-		name = newName;
+		// consider if both local and instance varaibles has same then we can use this(keyword) to call instance varaible
+		// like this this.instance_var = local_var
+		
+		this.name = name;
 		
 	}
 	
