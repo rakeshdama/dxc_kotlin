3239c0f318a7622577d547a823122e226139c9a1
rakeshdama <rakeshd0552@gmail.com>
2021-04-09 12:56:20 +0530

    Supplier Interface

---
+++ dxc_java/src/advanced/inbuilt_functionalinterfaces/supplier/SupplierTest.java
@@ -0,0 +1,23 @@
+package advanced.inbuilt_functionalinterfaces.supplier;
+
+
+import java.util.Date;
+import java.util.function.Supplier;
+
+public class SupplierTest {
+
+	public static void main(String[] args) {
+		
+		Supplier<Date> s = () -> new Date();
+		System.out.println(s.get());
+		
+		Supplier<String> otpSupplier = () -> {
+			String otp = "";
+			for (int i=0;i<6;i++) {
+				otp = otp+ (int)(Math.random()*10);
+			}
+			return otp;
+		};
+		System.out.println(otpSupplier.get());
+	}
+}
