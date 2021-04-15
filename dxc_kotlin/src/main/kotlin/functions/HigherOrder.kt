cf8a8dc72494e42d841bc26e0cae4de2a9a448a9
rakeshdama <rakeshd0552@gmail.com>
2021-04-15 11:33:12 +0530

    function of function

--- dxc_kotlin/src/main/kotlin/functions/HigherOrder.kt
+++ dxc_kotlin/src/main/kotlin/functions/HigherOrder.kt
@@ -1,8 +1,5 @@
 package functions
 
-class HigherOrder {
-}
-
 fun add(a:Int, b: Int):Int{
     return a+b
 }
@@ -14,12 +11,12 @@ fun calculate(x : Int, y : Int, operation : (Int, Int) -> Int ):Int{
 }
 
 fun main() {
-    var resultAdd = calculate(10,20, ::add)
+    val resultAdd = calculate(10,20, ::add)
     println(resultAdd)
 
-    var resultMul = calculate(10,20,::mul)
+    val resultMul = calculate(10,20,::mul)
     println(resultMul)
 
-    var resultMax = calculate(10,20,::maxOf)
+    val resultMax = calculate(10,20,::maxOf)
     println(resultMax)
 }
