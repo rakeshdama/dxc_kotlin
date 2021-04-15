0863c7345930d4064d433e74003bdef131c7a4ac
rakeshdama <rakeshd0552@gmail.com>
2021-04-15 15:07:34 +0530

    Sets Demo

---
+++ dxc_kotlin/src/main/kotlin/collections/SetsDemo.kt
@@ -0,0 +1,29 @@
+package collections
+
+class SetsDemo {
+  fun addIssue(str: String, issue: MutableSet<String>): Boolean {
+        if (issue.add(str)) {
+            println("Issue $str added")
+            return true
+        }
+        else {
+            println("Issue $str alread exists")
+            return false
+        }
+  }
+
+    fun printIssues(issue: MutableSet<String>){
+        for (i in issue){
+            println(i)
+        }
+    }
+}
+
+fun main() {
+    val issues: MutableSet<String> = mutableSetOf("1", "2", "3")
+    val demosets = SetsDemo()
+    demosets.addIssue("1",issues)
+    demosets.addIssue("Other",issues)
+    demosets.printIssues(issues)
+
+}
