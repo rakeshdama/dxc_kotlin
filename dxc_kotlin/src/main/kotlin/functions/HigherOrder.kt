66e54855338b281f5087599b13e1d3896e5e9cdc
rakeshdama <rakeshd0552@gmail.com>
2021-04-15 11:47:29 +0530

    function of function

--- dxc_kotlin/src/main/kotlin/functions/HigherOrder.kt
+++ dxc_kotlin/src/main/kotlin/functions/HigherOrder.kt
@@ -10,6 +10,12 @@ fun calculate(x : Int, y : Int, operation : (Int, Int) -> Int ):Int{
     return operation(x,y)
 }
 
+fun demoFunc(): (Int) -> Int {
+    return :: square
+}
+
+fun square(x: Int) = x*x
+
 fun main() {
     val resultAdd = calculate(10,20, ::add)
     println(resultAdd)
@@ -19,4 +25,10 @@ fun main() {
 
     val resultMax = calculate(10,20,::maxOf)
     println(resultMax)
+
+    val func = demoFunc()
+    println(func(10))
+
 }
+
+
