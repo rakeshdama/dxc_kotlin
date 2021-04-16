8d13d083975d9c838b14216d3637b74e1ee2743f
rakeshdama <rakeshd0552@gmail.com>
2021-04-16 12:09:35 +0530

    Delegation

---
+++ dxc_kotlin/src/main/kotlin/delegation/Delegation.kt
@@ -0,0 +1,20 @@
+package delegation
+
+interface  SoundBehaviour{
+    fun makeSound()
+}
+
+class ScreamBehaviour ( val scream : String) : SoundBehaviour {
+    override fun makeSound() {
+        println(scream.toUpperCase())
+    }
+}
+
+class Singer (val song:String) : SoundBehaviour by ScreamBehaviour(song){
+
+}
+
+fun main() {
+    val singer = Singer("Rakesh")
+    singer.makeSound()
+}
