725827ec9c23da99ccb22d4eb44fb632c265c3ff
rakeshdama <rakeshd0552@gmail.com>
2021-04-19 17:18:39 +0530

    First Spring

--- first_spring/src/first_spring/Test.java
+++ first_spring/src/first_spring/Test.java
@@ -3,14 +3,22 @@ package first_spring;
 import org.springframework.beans.factory.BeanFactory;  
 import org.springframework.beans.factory.xml.XmlBeanFactory;  
 import org.springframework.core.io.ClassPathResource;  
-import org.springframework.core.io.Resource;  
+import org.springframework.core.io.Resource;
+
+import constructor.injection.Question;  
 
 public class Test {  
 	public static void main(String[] args) {  
 		Resource resource=new ClassPathResource("applicationContext.xml");  
 		BeanFactory factory=new XmlBeanFactory(resource);  
 
-		Student student=(Student)factory.getBean("studentbean");  
-		student.displayInfo();  
+//		Student student=(Student)factory.getBean("studentbean");  
+//		student.displayInfo(); 
+//		
+//		Employee s = (Employee)factory.getBean("employee");
+//		s.show();
+//		
+		Question q = (Question)factory.getBean("q");
+		q.displayInfo();
 	}  
 }  
